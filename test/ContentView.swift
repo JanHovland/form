c0464d69d7af12b0c40e1cadd8abb5b35a80e13d
Jan Hovland <jan.hovland@lyse-net>
2019-10-19 10:27:11 +0200

    Update

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -19,29 +19,30 @@ struct ContentView : View {
     @State private var password: String = ""
     @State private var password_two: String = ""
     @State private var password_three: String = ""
+    @State private var password_four: String = ""
 
     var body: some View {
         NavigationView {
             Form {
-                Section  {
-                    Text("Name")
+                Section (header: Text("Name")) {
+                    // Text("Name")
                     TextField("Name", text: $name)
                 }
-                Section  {
-                    Text("eMail")
+                Section (header: Text("eMail")) {
                     TextField("Email", text: $email)
                 }
-                Section  {
-                    Text("Passord")
+                Section (header: Text("Passord")) {
                     SecureField("Legg inn passord", text: $password)
                 }
-                Section {
-                    Text("Passord two")
+                Section (header: Text("Passord two")
+                    .padding(5).lineLimit(nil)) {
                     SecureField("Legg inn passord two", text: $password_two)
                 }
-                Section {
-                    Text("Passord three")
-                    SecureField("Legg inn passord two", text: $password_three)
+                Section (header: Text("Passord three")) {
+                    SecureField("Legg inn passord three", text: $password_three)
+                }
+                Section (header: Text("Passord four")) {
+                    SecureField("Legg inn passord four", text: $password_four)
                 }
             }
             .navigationBarTitle(Text("Person opplysninger"))
